d67521feee153ee480caae6f016ceca2f73fca7d
Stefan Dierauf <sdierauf@gmail.com>
2015-08-15 10:38:07 -0700

    No more hacky js here

--- hangman.js
+++ hangman.js
@@ -1,3 +1,8 @@
+/**
+ * Stefan Dierauf 2014
+ * JS implementation of EvilHangman
+ */
+
 var guessesbox = document.getElementById('guesses');
 var submit = document.getElementById('guessbutton');
 var guesschar = document.getElementById('guesschar');
@@ -13,182 +18,226 @@ Array.prototype.randomElement = function() {
   return this[Math.floor(Math.random() * this.length)];
 };
 
-function HangmanManager(numguesses, thelength) {
-  var curDictionary = stripDict(dictionary, thelength);
-  
-  var guesscount = numguesses;
-  this.getGuessCount = function() { return guesscount };
-  this.length = thelength;
-  var guesses = [];
-  this.getGuesses = function() {
-    var string = "";
-    for (var i = 0; i < guesses.length; i++) {
-      string += guesses[i] + ", ";
-    }
-    return string;
-  }
-  //I don't understand js scope ;-;
-  var getGuesses = function() {
-    var string = "";
-    for (var i = 0; i < guesses.length; i++) {
-      string += guesses[i] + ", ";
-    }
-    return string;
-  }
-  
-  this.callRedraw = function() {
-    redraw();
-  }
-  
-  var activePattern = "";
+/**
+ * Manages the evil hangman game.
+ * @param numguesses The number of guesses the user has.
+ * @param length The length of the current word.
+ */
+function HangmanManager(numguesses, length) {
+  this.guesscount = numguesses;  // Save the number of guesses.
+  this.length = length;  // Save the word length.
+  // Get all the words out of the global dictionary of a certain length.
+  this.curDictionary = window.dictionary.filter(
+      function (element) {
+        return element.length == this.length;
+      },
+      this
+    );
+  this.guesses = [];  // Char array of the guessed letters.
+  this.activePattern = "";  // The drawn pattern in the dom.
   for (var i = 0; i < this.length; i++) {
     if (i != 0) {
-      activePattern += " ";
+      this.activePattern += " ";
     }
-    activePattern += "__";
+    this.activePattern += "__";
   }
-  result.innerHTML = activePattern;
-  
-  this.getActivePattern = function() { return activePattern };
-  
-  this.words = function() {
-    return curDictionary;
-  }
-  
-  this.play = function(character) {
-    //guesscount--;
-    var families = this.findFamilies(character);
-    var largest = "unassigned";
-    var keys = Object.keys(families);
-    for (var i = 0; i < keys.length; i++) {
-      //console.log(keys[i]);
-      if (largest == "unassigned" || families[keys[i]].length > families[largest].length) {
-        largest = keys[i];
-      }
+  result.innerHTML = this.activePattern;
+  this.redraw();
+}
+
+
+/**
+ * Takes a word, a letter, then builds a word family pattern out of the current
+ * active pattern + the word. For example, if word = apple, letter = a, and the
+ * activePattern is '__ p p __ __', returns 'a p p __ __'. Requires that the
+ * passed word matches the activeFamily pattern.
+ * @param word
+ * @param letter
+ * @return {string}
+ */
+HangmanManager.prototype.famPatternBuilder = function (word, letter) {
+  var family = "";
+  for (var j = 0; j < word.length; j++) {
+    if (j != 0) {
+      family += " ";
     }
-    
-    console.log("biggest was " + largest + " with " + families[largest]);
-    
-    var count = 0;
-    for (var j = 0; j < largest.length; j++) {
-      if (largest.charAt(j) == character) {
-        count++;
-      }
-    }
-    /*
-if (count > 0) {
-      this.guesses.push(character);
-    }
-*/
-    
-    update(character, largest, families);
-    redraw();
-    
-    return count;
-    
-  }
-  
-  var redraw = function() {
-  	if (guesscount == 0) {
-    	if (activePattern.indexOf("_") > -1) {
-	    	activePattern = curDictionary.randomElement();
-    	}
-      result.classList.add("text-danger");
-      header.innerHTML = "You lost!";
-    }
-    console.log(guesscount);
-    guessesbox.innerHTML = getGuesses();
-    result.innerHTML = activePattern;
-    
-    if (debug.checked == true) {
-      debugoutput.innerHTML = curDictionary;
+    if (word.charAt(j) == letter) {
+      family += letter;
+    } else if (this.activePattern.indexOf(word.charAt(j)) != -1) {
+      family += word.charAt(j);
     } else {
-      debugoutput.innerHTML = "";
+      family += "__";
     }
-    guessesleft.innerHTML = guesscount;
   }
-  
-  var update = function(letter, largest, families) {
-    if (activePattern == largest) {
-      guesscount--;
+  return family;
+};
+
+
+/**
+ * @return {string} The active pattern.
+ */
+HangmanManager.prototype.getActivePattern = function () {
+  return this.activePattern;
+};
+
+
+/**
+ * @return {number} The number of guesses remaining.
+ */
+HangmanManager.prototype.getGuessCount = function () {
+  return this.guesscount;
+};
+
+
+/**
+ * @return {string} A formatted string of all the guessses.
+ */
+HangmanManager.prototype.getGuesses = function () {
+  var string = "";
+  for (var i = 0; i < this.guesses.length; i++) {
+    string += this.guesses[i] + ", ";
+  }
+  return string;
+};
+
+
+/**
+ * Takes a character and 'guesses' it against EvilHangman. Returns the number
+ * of words in the dictionary that make a pattern with the largest set of
+ * matching words.
+ * @param {string} character
+ * @return {number}
+ */
+HangmanManager.prototype.play = function (character) {
+  var families = this.findFamilies(character);
+  var largest = "unassigned";
+  var keys = Object.keys(families);
+  for (var i = 0; i < keys.length; i++) {
+    if (largest == "unassigned" || families[keys[i]].length > families[largest].length) {
+      largest = keys[i];
     }
-    activePattern = largest;
-    curDictionary = families[largest];
-    guesses.push(letter);
   }
-  
-  this.findFamilies = function(letter) {
-    var families = {};
-    console.log(curDictionary.length);
-    for (var i = 0; i < curDictionary.length; i++) {
-      var word = curDictionary[i];
-      //console.log(word);
-      var famPattern = famPatternBuilder(word, letter);
-      //console.log(famPattern);
-      if (families[famPattern] == undefined) {
-        families[famPattern] = Array(word);
-      } else {
-        //console.log("this happened");
-        families[famPattern].push(word);
-      }
+  var count = 0;
+  for (var j = 0; j < largest.length; j++) {
+    if (largest.charAt(j) == character) {
+      count++;
     }
-    return families;
   }
-  
-  var famPatternBuilder = function(word, letter) {
-    var family = "";
-    for (var j = 0; j < word.length; j++) {
-      if (j != 0) {
-        family += " ";
-      }
-      if (word.charAt(j) == letter) {
-        family += letter;
-      } else if (activePattern.indexOf(word.charAt(j)) != -1) {
-        console.log("checked active pattern");
-        family += word.charAt(j);
-      } else {
-        family += "__";
-      }
+  this.update(character, largest, families);
+  this.redraw();
+  return count;
+};
+
+
+/**
+ * Updates the UI with the current state of the game.
+ */
+HangmanManager.prototype.redraw = function () {
+  if (this.guesscount == 0) {
+    if (this.activePattern.indexOf("_") > -1) {
+      this.activePattern = this.curDictionary.randomElement();
     }
-    return family;
+    result.classList.add("text-danger");
+    header.innerHTML = "You lost!";
   }
-  
-  redraw();
-  
+  console.log(this.guesscount);
+  guessesbox.innerHTML = this.getGuesses();
+  result.innerHTML = this.activePattern;
+
+  // Update debug view.
+  if (debug.checked) {
+    debugoutput.innerHTML = this.curDictionary;
+  } else {
+    debugoutput.innerHTML = "";
+  }
+  guessesleft.innerHTML = this.guesscount;
+};
+
+
+/**
+ * Takes a letter and returns a map of all possible family patterns 
+ * to an array of their matching words.
+ * @param {string} letter
+ * @return Map<{string}, Array<{String}>>
+ */
+HangmanManager.prototype.findFamilies = function(letter) {
+  var families = {};
+  for (var i = 0; i < this.curDictionary.length; i++) {
+    var word = this.curDictionary[i];
+    var famPattern = this.famPatternBuilder(word, letter);
+    if (families[famPattern] == undefined) {
+      families[famPattern] = Array(word);
+    } else {
+      families[famPattern].push(word);
+    }
+  }
+  return families;
 }
 
-var stripDict = function(dict, num) {
-  var newDict = [];
-  for (var i = 0; i < dict.length; i++) {
-    if (dict[i].length == num) {
-      newDict.push(dict[i]);
-    }
-  }
-  return newDict;
-}
 
-var hangmanGen = function() {
+/**
+ * 
+ */
+HangmanManager.prototype.update = function (letter, largest, families) {
+  // If the active pattern didn't change with the new guess,
+  // the letter was not part of it, so decrement guesscount.
+  if (this.activePattern == largest) {
+    this.guesscount--;
+  }
+  this.activePattern = largest;
+  this.curDictionary = families[largest];
+  this.guesses.push(letter);
+};
+
+
+/**
+ * @return {Array<string>} the current set of words EvilHangman is
+ * 'guessing' from.
+ */
+HangmanManager.prototype.words = function () {
+  return this.curDictionary;
+};
+
+
+/**
+ * @return {HangmanManager} a new EvilHangman game with a random
+ * word length and guess amount.
+ */
+function HangmanGen () {
   var guesses = Math.floor((Math.random() * 3) + 5);
   var letters = Math.floor((Math.random() * 3) + 4);
   return new HangmanManager(guesses, letters);
-}
+};
 
 
-var hangman = hangmanGen();
+var hangman = HangmanGen();
 
-debug.onclick = function() {
-  hangman.callRedraw();
-}
 
-window.onkeypress = function(event) {
+/**
+ * Clicking debug should update the state to show the debug output
+ */
+debug.onclick = function () {
+  hangman.redraw();
+};
+
+
+/**
+ * Call submit on pressing the return key.
+ */
+window.onkeypress = function (event) {
   if (event.keyCode == 13) {
     submit.onclick();
   }
-}
+};
 
 
-submit.onclick = function() {
+
+/**
+ * Manage UI on a guess.
+ */
+submit.onclick = function () {
   if (hangman.getGuessCount() == 0) {
+    // Reload the page to play a new game.
     location.reload();
   } else {
     if (guesschar.value.length == 1) {
@@ -204,16 +253,22 @@ submit.onclick = function() {
     guesschar.value = "";
     guesschar.focus();
   }
-}
+};
 
-tryagain.onclick = function() {
+
+/**
+ * Clicking the try again button should reload the page.
+ */
+tryagain.onclick = function () {
   location.reload();
-}
+};
 
-window.onload = function() {
+
+/**
+ * Focus on the guess input after a page load.
+ */
+window.onload = function () {
   guesschar.focus();
-}
+};
 
-//console.log(hangman.words());
-console.log(hangman.getActivePattern());
 
